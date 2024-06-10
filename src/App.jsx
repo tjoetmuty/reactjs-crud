8c7a57ed0c3fe0bdd50a5f420471cbd54743861b
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-06-10 18:37:56 +0800

    adding new folder and making props

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import UserList from './features/Users/UserList'
 
@@ -5,8 +6,12 @@ import UserList from './features/Users/UserList'
 function App() {
 
   return (
-  <div>
-    <UserList/>
+  <div className='container mx-auto px-2 max-w-5xl pt-10 md:pt-32'>
+    <h1 className='text-center text-gray-700 text-2xl font-bold'>CRUD with redux toolkit</h1>
+    <Routes>
+      <Route path='/' element={<UserList/>}/>
+    </Routes>
+  
   </div>
   )
 }
